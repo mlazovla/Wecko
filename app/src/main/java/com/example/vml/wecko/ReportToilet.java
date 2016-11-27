016a2dcb8649d036162711cd8a84a0d06c706cd2
mlazovla <mlazovla@gmail.com>
2016-11-27 22:00:27 +0100

    Add activities: add toilet, report toilet. Link float button to add new toilet.

---
+++ app/src/main/java/com/example/vml/wecko/ReportToilet.java
@@ -0,0 +1,15 @@
+package com.example.vml.wecko;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class ReportToilet extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_report_toilet);
+
+    }
+}
