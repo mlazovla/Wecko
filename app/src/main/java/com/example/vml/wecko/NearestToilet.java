10a6e159ef0971e1b1f2010f8c864e316a40e0ed
kipci <kipci>
2016-11-27 16:03:42 +0100

    adds images and layout

--- app/src/main/java/com/example/vml/wecko/NearestToilet.java
+++ app/src/main/java/com/example/vml/wecko/NearestToilet.java
@@ -24,7 +24,6 @@ public class NearestToilet extends AppCompatActivity
         setSupportActionBar(toolbar);
 
 
-
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
