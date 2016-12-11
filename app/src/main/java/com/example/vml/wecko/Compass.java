3a9e09e221c8e753f43dc3e775c16116da7cc4fa
mlazovla <mlazovla@gmail.com>
2016-12-11 19:56:42 +0100

    Add Compass screen. Fix some shit around.
    Merge branch 'master' of https://github.com/mlazovla/Wecko
    
    # Conflicts:
    #	app/src/main/java/com/example/vml/wecko/NearestToilet.java

--- app/src/main/java/com/example/vml/wecko/Compass.java
+++ app/src/main/java/com/example/vml/wecko/Compass.java
@@ -1,15 +1,64 @@
 package com.example.vml.wecko;
 
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Compass extends AppCompatActivity {
+public class Compass extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager mSensorManager;
+    Sensor accelerometer;
+    Sensor magnetometer;
+    ImageView compassDirection;
+    TextView worldDirection;
 
 
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_compass);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+        compassDirection = (ImageView) this.findViewById(R.id.compassDirection);
+        worldDirection = (TextView) this.findViewById(R.id.worldDirection);
 
+    }
+
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_UI);
+        mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_UI);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
+
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {  }
+
+    float[] mGravity;
+    float[] mGeomagnetic;
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+            mGravity = event.values;
+        if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+            mGeomagnetic = event.values;
+        if (mGravity != null && mGeomagnetic != null) {
+            float R[] = new float[9];
+            float I[] = new float[9];
+            boolean success = SensorManager.getRotationMatrix(R, I, mGravity, mGeomagnetic);
+            if (success) {
+                float orientation[] = new float[3];
+                SensorManager.getOrientation(R, orientation);
+                float azimut = orientation[0]; // orientation contains: azimut, pitch and roll
+                this.compassDirection.setRotation(-57*azimut);
+            }
+        }
     }
 }
